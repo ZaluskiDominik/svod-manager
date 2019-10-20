1275373ff72e1a9b505190b6582b9b3a505ed3b0
ZaluskiDominik <d.zaluski98@gmail.com>
2019-10-20 14:05:49 +0200

    Added first files

---
+++ src/Controller/View/LoginPageController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controller\View;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LoginPageController extends AbstractController
+{
+    /** @Route("/login/customer", methods={GET}) */
+    public function customerLoginPageAction()
+    {
+
+    }
+}
